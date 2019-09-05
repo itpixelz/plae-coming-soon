1a982a5a8350246df6b665dbeda8f516c92a1633
Fahad Murtaza <fahdim@gmail.com>
2019-09-05 19:11:35 +0400

    Updated the date

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 ﻿jQuery(document).ready(function() {
   jQuery('.page-loader').fadeOut();
   jQuery('.countdown').countdown({
-    date: 'June 7, 2087 15:03:26',
+    // date: 'June 7, 2087 15:03:26',
+    date: 'September 18, 2019 12:00:00',
+    //TODO: might need formatting and also get it from the data
   });
 });
 
