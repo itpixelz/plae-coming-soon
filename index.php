7b62291977537ebe299901e9b681d18a6d9b19e4
Fahad Murtaza <fahdim@gmail.com>
2019-09-05 19:06:19 +0400

    Turned it to a wp theme with contact for button updated

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=no">
+    <meta name="viewport"
+          content="width=device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="True">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -14,20 +15,22 @@
     <title>PLAE - Coming Soon </title>
 
     <!-- ============ GOOGLE FONTS ============ -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600'
+          rel='stylesheet' type='text/css'>
 
     <!-- CSS -->
     <!-- Animate css -->
-    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/css/animate.css">
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/font-awesome/css/font-awesome.min.css"/>
     <!-- Custom styles CSS -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/css/style.css">
     <!-- Bootstrap v3.3.1 -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/css/bootstrap.min.css">
     <!-- Favicons -->
-    <link rel="shortcut icon" href="images/favicon.ico.png">
+    <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory');?>images/favicon.ico.png">
 
 </head>
 
@@ -35,7 +38,8 @@
 
 <!--= LOADER TEMPLATE -->
 <div class="page-loader">
-    <div class="loader-icon"><img style="width: 100px; height: 100px" src="images/spinner.svg" alt=""></div>
+    <div class="loader-icon"><img style="width: 100px; height: 100px"
+                                  src="images/spinner.svg" alt=""></div>
 </div>
 <!-- /LOADER TEMPLATE -->
 
@@ -47,7 +51,8 @@
     <!--<div id="video" data-video="cphYpNus4oQ" data-mute="true"></div>-->
     <!-- /YOUTUBE PLAYER -->
 
-    <div class="logo"><img src="images/logo.svg" width="190" height="190" alt=""></div>
+    <div class="logo"><img src="images/logo.svg" width="190" height="190"
+                           alt=""></div>
 
     <div class="coming-soon animated">
 
@@ -55,21 +60,60 @@
 
         <!-- COUNTDOWN -->
         <div class="countdown">
-            <div id="countdown-plae" class="countdowns" ><span class="ult_countdown-section"><span class="ult_time-mid"><span class="ult_countdown-amount ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-amount" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:50px;tablet:45px;tablet_portrait:35px;mobile_landscape:25px;mobile:20px;&quot;,&quot;line-height&quot;:&quot;&quot;}" style="background: rgb(0, 0, 0); padding: 0px;">00</span><span class="ult_countdown-period ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-period" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:13px;&quot;,&quot;line-height&quot;:&quot;&quot;}">Days</span></span></span><span class="ult_countdown-section"><span class="ult_time-mid"><span class="ult_countdown-amount ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-amount" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:50px;tablet:45px;tablet_portrait:35px;mobile_landscape:25px;mobile:20px;&quot;,&quot;line-height&quot;:&quot;&quot;}" style="background: rgb(0, 0, 0); padding: 0px;">00</span><span class="ult_countdown-period ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-period" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:13px;&quot;,&quot;line-height&quot;:&quot;&quot;}">Hours</span></span></span><span class="ult_countdown-section"><span class="ult_time-mid"><span class="ult_countdown-amount ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-amount" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:50px;tablet:45px;tablet_portrait:35px;mobile_landscape:25px;mobile:20px;&quot;,&quot;line-height&quot;:&quot;&quot;}" style="background: rgb(0, 0, 0); padding: 0px;">00</span><span class="ult_countdown-period ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-period" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:13px;&quot;,&quot;line-height&quot;:&quot;&quot;}">Minutes</span></span></span><span class="ult_countdown-section"><span class="ult_time-mid"><span class="ult_countdown-amount ult-responsive" data-ultimate-target="#count-down-wrap-4726 .ult_countdown-amount" data-responsive-json-new="{&quot;font-size&quot;:&quot;desktop:50px;tablet:45px;tablet_portrait:35px;mobile_landscape:25px;mobile:20px;&quot;,&quot;line-height&quot;:&quot;&quot;}" style="background: rgb(0, 0, 0); padding: 0px;">00</span><span class="ult_countdown-period ult-responsive">Seconds</span></span></span></span></div>
+            <div id="countdown-plae" class="countdowns">
+                <span class="ult_countdown-section">
+                    <span class="ult_time-mid">
+                        <span class="ult_countdown-amount ult-responsive">
+                            00
+                        </span>
+                        <span class="ult_countdown-period ult-responsive">
+                            Days
+                        </span>
+                    </span>
+                </span>
+
+                <span class="ult_countdown-section">
+                    <span class="ult_time-mid">
+                        <span class="ult_countdown-amount ult-responsive">
+                            00
+                        </span>
+                        <span class="ult_countdown-period ult-responsive">
+                            Hours
+                        </span>
+                    </span>
+                </span>
+
+                <span class="ult_countdown-section">
+                    <span class="ult_time-mid">
+                        <span class="ult_countdown-amount ult-responsive">
+                            00
+                        </span>
+                        <span class="ult_countdown-period ult-responsive">
+                            Minutes
+                        </span>
+                    </span>
+                </span>
+
+                <span class="ult_countdown-section">
+                    <span class="ult_time-mid">
+                        <span class="ult_countdown-amount ult-responsive">
+                            00
+                        </span>
+                        <span class="ult_countdown-period ult-responsive">
+                            Seconds
+                        </span>
+                    </span>
+                </span>
+                </span>
+            </div>
         </div>
         <!-- /COUNTDOWN -->
 
-        <a class="whiteghost" href="#contact"> CONTACT </a>
+        <a href="#contact">
+            <button class="whiteghost"> CONTACT</button>
+        </a>
 
     </div>
-
-    <!-- MOUSE ICON -->
-    <!--<div class="mouse">
-        <a href="#services">
-            <i class="fa fa-chevron-down"></i>
-        </a>
-    </div>-->
-    <!-- /MOUSE ICON -->
 
 
 </header>
@@ -80,15 +124,23 @@
 <section class="contact" id="contact">
     <div class="container">
         <div class="row">
-            <h2 data-sr="wait 0.5s, then enter top and move 40px over 1s"><span>CONTACT US</span></h2>
+            <h2 data-sr="wait 0.5s, then enter top and move 40px over 1s"><span>CONTACT US</span>
+            </h2>
             <div class="col-md-12">
                 <div id="note"></div>
                 <div id="fields">
                     <form id="ajax-contact-form">
-                        <input type="text" name="name" id="name" placeholder="Name" value="" data-sr="enter left move 25px, after 0.3s"/>
-                        <input type="email" name="email" id="email" placeholder="Email" value="" data-sr="enter right move 25px, after 0.3s"/>
-                        <textarea name="msg" id="msg" placeholder="Message" data-sr="enter bottom move 25px, after 0.3s"></textarea>
-                        <input type="submit" name="submit" id="submit" value="Send message" data-sr="wait 0.8s, then enter top and move 40px after 0.3s"/>
+                        <input type="text" name="name" id="name"
+                               placeholder="Name" value=""
+                               data-sr="enter left move 25px, after 0.3s"/>
+                        <input type="email" name="email" id="email"
+                               placeholder="Email" value=""
+                               data-sr="enter right move 25px, after 0.3s"/>
+                        <textarea name="msg" id="msg" placeholder="Message"
+                                  data-sr="enter bottom move 25px, after 0.3s"></textarea>
+                        <input type="submit" name="submit" id="submit"
+                               value="Send message"
+                               data-sr="wait 0.8s, then enter top and move 40px after 0.3s"/>
                     </form>
                 </div>
             </div>
@@ -101,15 +153,15 @@
 
 <!-- Javascript files -->
 <!-- jQuery -->
-<script src="js/jquery.js"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/jquery.js"></script>
 
 <!-- CountDown  -->
-<script src="js/jquery.countdown.js"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/jquery.countdown.js"></script>
 <!-- Validate form -->
-<script src="js/jquery.validate.js"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/jquery.validate.js"></script>
 <!-- Ajax chimp -->
-<script src="js/jquery.ajaxchimp.js"></script>
-<script src="js/main.js"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/jquery.ajaxchimp.js"></script>
+<script src="<?php bloginfo('stylesheet_directory');?>/js/main.js"></script>
 </body>
 
 </html>
